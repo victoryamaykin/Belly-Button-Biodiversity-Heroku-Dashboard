2c2d74d93733030c455b2457519d707668424a34
Victor Yamaykin <victor.yamaykin@gmail.com>
2019-05-22 14:30:09 -0400

    edit gauge

--- bellybuttonbioversity/static/js/app.js
+++ bellybuttonbioversity/static/js/app.js
@@ -8,10 +8,11 @@ function buildMetadata(sample) {
       Object.entries(data).forEach(([key, value]) => {
         selector.append("h3").text(`${key}: ${value}`);
       });
+      buildGauge(data.WFREQ);
     }); 
   }
-// BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+// // BONUS: Build the Gauge Chart
+//     buildGauge(data.WFREQ);
 
 function buildCharts(sample) {
   d3.json(`/samples/${sample}`).then(function(data1) {
@@ -28,6 +29,7 @@ function buildCharts(sample) {
     var data1 = [trace1];
   
     var layout1 = {
+      title: `Bacteria Values for Sample: ${sample}`,
       showlegend: false,
       xaxis: {
         title: {
@@ -51,85 +53,20 @@ function buildCharts(sample) {
         labels: data2.otu_ids.slice(0,10),
         values: data2.sample_values.slice(0,10),
         type: "pie",
-        hoverinfo: data2.otu_labels.slice(0,10)
+        hoverinfo: data2.sample_values.slice(0,10)
       }
 
       var data2 = [trace2]
 
       var layout2 = {
         title: `Sample #: ${sample}`,
-        height: 400,
-        width: 500
+        height: 500,
+        width: 700
       };
       var PIE = document.getElementById("pie");
 
       Plotly.newPlot(PIE, data2, layout2, {responsive: true});
     })
-
-    // d3.jason(`/wfreq/${sample}`).then((data3) => {
-
-    //   // Enter a speed between 0 and 180
-    //     var level = 175;
-
-    //     // Trig to calc meter point
-    //     var degrees = 180 - level,
-    //         radius = .5;
-    //     var radians = degrees * Math.PI / 180;
-    //     var x = radius * Math.cos(radians);
-    //     var y = radius * Math.sin(radians);
-
-    //     // Path: may have to change to create a better triangle
-    //     var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-    //         pathX = String(x),
-    //         space = ' ',
-    //         pathY = String(y),
-    //         pathEnd = ' Z';
-    //     var path = mainPath.concat(pathX,space,pathY,pathEnd);
-
-    //     var data3 = [{ type: 'scatter',
-    //       x: [0], y:[0],
-    //         marker: {size: 28, color:'850000'},
-    //         showlegend: false,
-    //         name: 'speed',
-    //         text: level,
-    //         hoverinfo: 'text+name'},
-    //       { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
-    //       rotation: 90,
-    //       text: ['0-1', '1-2', 'Fast', 'Average',
-    //                 'Slow', 'Super Slow', ''],
-    //       textinfo: 'text',
-    //       textposition:'inside',
-    //       marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-    //                             'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
-    //                             'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
-    //                             'rgba(255, 255, 255, 0)']},
-    //       labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
-    //       hoverinfo: 'label',
-    //       hole: .5,
-    //       type: 'pie',
-    //       showlegend: false
-    //     }];
-
-    //     var layout3 = {
-    //       shapes:[{
-    //           type: 'path',
-    //           path: path,
-    //           fillcolor: '850000',
-    //           line: {
-    //             color: '850000'
-    //           }
-    //         }],
-    //       title: '<h3><b>Belly Button Washing Frequency</b></h3> <br> Scrubs per Week',
-    //       height: 1000,
-    //       width: 1000,
-    //       xaxis: {zeroline:false, showticklabels:false,
-    //                 showgrid: false, range: [-1, 1]},
-    //       yaxis: {zeroline:false, showticklabels:false,
-    //                 showgrid: false, range: [-1, 1]}
-    //     };
-    //     GAUGE = document.getElementById("gauge")
-    // //     Plotly.newPlot(GAUGE, data3, layout3, {responsive: true});
-    // })
 }
 
 function init() {
---
+++ bellybuttonbioversity/static/js/bonus.js
@@ -0,0 +1,73 @@
+function buildGauge (sample) {
+   
+
+        // Enter a speed between 0 and 180
+            var level = parseFloat(sample) * 20;
+
+            // Trig to calc meter point
+            var degrees = 180 - level,
+                radius = .5;
+            var radians = degrees * Math.PI / 180;
+            var x = radius * Math.cos(radians);
+            var y = radius * Math.sin(radians);
+
+            // Path: may have to change to create a better triangle
+            var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+                pathX = String(x),
+                space = ' ',
+                pathY = String(y),
+                pathEnd = ' Z';
+            var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+            var data3 = [{ type: 'scatter',
+            x: [0], y:[0],
+                marker: {size: 28, color:'850000'},
+                showlegend: false,
+                name: `Wash Frequency:${sample}`,
+                text: level,
+                hoverinfo: 'text+name'},
+            { values: [50/9, 50/9, 50/9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+            rotation: 90,
+            text: ['8-9', '7-8', '6-7', '5-6',
+                        '4-5', '3-4', '2-3', '1-2', '0-1'],
+            textinfo: 'text',
+            textposition:'inside',
+            marker: {colors: [
+                "rgba(0, 105, 11, .5)",
+                "rgba(10, 120, 22, .5)",
+                "rgba(14, 127, 0, .5)",
+                "rgba(110, 154, 22, .5)",
+                "rgba(170, 202, 42, .5)",
+                "rgba(202, 209, 95, .5)",
+                "rgba(210, 206, 145, .5)",
+                "rgba(232, 226, 202, .5)",
+                "rgba(240, 230, 215, .5)",
+                "rgba(255, 255, 255, 0)"
+              ]},
+            labels: ['8-9', '7-8', '6-7', '5-6','4-5', '3-4', '2-3', '1-2', '0-1'],
+            hoverinfo: 'label',
+            hole: .5,
+            type: 'pie',
+            showlegend: false
+            }];
+
+            var layout3 = {
+            shapes:[{
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                }
+                }],
+            title: `<b>Belly Button Washing Frequency</b> <br> Scrubs per Week: ${sample}`,
+            height: 1000,
+            width: 1000,
+            xaxis: {zeroline:false, showticklabels:false,
+                        showgrid: false, range: [-1, 1]},
+            yaxis: {zeroline:false, showticklabels:false,
+                        showgrid: false, range: [-1, 1]}
+            };
+            GAUGE = document.getElementById("gauge")
+            Plotly.newPlot(GAUGE, data3, layout3, {responsive: true});
+}; 
